88b51ab0bc9f0307557d8fba365653475411c177
Mason Manderanch <mmandernach13@gmail.com>
2026-01-30 16:40:36 -0600

    added payload skeleton

--- ros2_ws/src/payload/payload/payload_control_node.py
+++ ros2_ws/src/payload/payload/payload_control_node.py
@@ -1,5 +1,16 @@
+from rclpy import Node
 
+# Placeholder for the PayloadControlNode class
+# need to be able to pick which payload to control
+# bottle or beacon. make the server side of the service
 
+class PayloadControlNode(Node):
+    def __init__(self):
+        super().__init__('payload_control_node')
+        # Initialization code here
+
+    def control_payload(self):
+        pass
 
 def main():
     pass
