d8f422b98f34f557e31c29fea5fcf84f746aad61
Mason Manderanch <mmandernach13@gmail.com>
2026-01-30 16:47:46 -0600

    added vision skeleton

--- ros2_ws/src/vision/vision/obstacle_detect_node.py
+++ ros2_ws/src/vision/vision/obstacle_detect_node.py
@@ -1,8 +1,19 @@
+from rclpy import Node
 
+# Placeholder for the ObstacleDetectNode class
+# This node will handle obstacle detection using vision data
+# make a node that "takes a pic" at some frequency and processes it
 
+class ObstacleDetectNode(Node):
+    def __init__(self):
+        super().__init__('obstacle_detect_node')
+        # Initialization code here
+
+    def timer_take_picture(self):
+        pass
+
+    def cam_detect_obstacles(self):
+        pass
 
 def main():
     pass
-
-if __name__ == "__main__":
-    main()
